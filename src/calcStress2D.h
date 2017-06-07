e55a5cf84d9c7ee0b1582dadf2ee1e59927be251
Amloris <aaronrob@ksu.edu>
2017-06-06 21:05:31 -0500

    Finsihed calcStress2D class

--- src/calcStress2D.h
+++ src/calcStress2D.h
@@ -38,6 +38,80 @@ public:
 //-----------------------------------------------------------------------------
 calcStress2D::calcStress2D()  { /*std::cout << "Creating calcStress2D Class Object" << endl;*/ }
 calcStress2D::~calcStress2D() { std::cout << "Deleting calcStress2D Class Object" << endl; }
+calcStress2D::calcStress2D(double xx, double yy, double zz, double xy)
+{
+	m_sigxx = xx;
+	m_sigyy = yy;
+	m_sigzz = zz;
+	m_sigxy = xy;
+}
 
+void calcStress2D::setSigXX(double xx)
+{	
+	m_sigxx = xx;                 //Set m_sigxx = xx
+}
+
+void calcStress2D::setSigYY(double yy)
+{
+	m_sigyy = yy;                 //Set m_sigyy = yy
+}
+
+void calcStress2D::setSigZZ(double zz)
+{
+	m_sigzz = zz;                 //Set m_sigzz = zz
+}
+
+void calcStress2D::setSigXY(double xy)	  
+{ 
+	m_sigxy = xy;                 //set m_sigxy = xy
+}
+
+void calcStress2D::addSigXX(double xx)	  
+{
+	m_sigxx += xx;                //Add xx to m_sigxx
+}
+
+void calcStress2D::addSigYY(double yy)	  
+{
+	m_sigyy += yy;                //Add yy to m_sigyy
+}
+
+void calcStress2D::addSigZZ(double zz)	  
+{
+	m_sigzz += zz;                //Add zz to m_sigzz
+}
+
+void calcStress2D::addSigXY(double xy)	  
+{
+	m_sigxy += xy;                //Add xy to m_sigxy
+}
+
+double calcStress2D::getSigXX() const	  
+{
+	return m_sigxx;               //Return m_sigxx
+}
+
+double calcStress2D::getSigYY() const	  
+{
+	return m_sigyy;               //Return m_sigyy
+}
+
+double calcStress2D::getSigZZ() const	  
+{
+	return m_sigzz;               //Return m_sigzz
+}
+
+double calcStress2D::getSigXY() const	  
+{
+	return m_sigxy;               //Return m_sigxy
+}
+
+void calcStress2D::zero()		  
+{
+	m_sigxx = 0.0;                //Initialize all components to zero
+	m_sigyy = 0.0;
+	m_sigzz = 0.0;
+	m_sigxy = 0.0;
+}
 
 #endif
